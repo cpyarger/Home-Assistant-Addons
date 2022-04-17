5e8545948835a69bb94b265bcd00f3144ba2e6cb
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-16 23:44:50 -0400

    trim config options

--- Git-Back-Up/gba.sh
+++ Git-Back-Up/gba.sh
@@ -3,10 +3,27 @@ export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
 CONFIG_PATH=/data/options.json
-REMOTE_REPO="$(jq --raw-output '.git_url' $CONFIG_PATH)"
-RSA_KEY="$(jq --raw-output '.git_rsa_kay' $CONFIG_PATH)"
-HA_ENABLE="$(jq --raw-output '.home_assistant_config_enable' $CONFIG_PATH)"
-ESPH_ENABLE="$(jq --raw-output '.esphome_config_enable' $CONFIG_PATH)"
+  include_secrets_file="$(jq --raw-output '.include_secrets_file' $CONFIG_PATH)"
+  deployment_key="$(jq --raw-output '.deployment_key' $CONFIG_PATH)"
+  deployment_key_protocol="$(jq --raw-output '.deployment_key_protocol' $CONFIG_PATH)"
+  deployment_password="$(jq --raw-output '.deployment_password' $CONFIG_PATH)"
+  deployment_user="$(jq --raw-output '.deployment_user' $CONFIG_PATH)"
+  git_branch="$(jq --raw-output '.git_branch' $CONFIG_PATH)"
+  git_remote="$(jq --raw-output '.git_remote' $CONFIG_PATH)"
+  repository="$(jq --raw-output '.repository' $CONFIG_PATH)"
+
+
+
+if [! -d /config/.git ]; then
+  echo Not yet a git repo;
+  #echo "# git-back-up" >> README.md
+  #git init
+  #git add README.md
+  #git commit -m "first commit"
+  #git branch -M main
+  #git remote add origin git@github.com:cpyarger/git-back-up-backup.git
+  #git push -u origin main
+fi;
 
 echo "Git Back Up Start"
 
