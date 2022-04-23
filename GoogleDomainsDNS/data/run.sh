3bb7d427d09135e693ac9a78f376672b54b4863b
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-22 21:07:06 -0400

    more secure method of posting to google

--- GoogleDomainsDNS/data/run.sh
+++ GoogleDomainsDNS/data/run.sh
@@ -47,9 +47,11 @@ while true; do
 
     [[ ${IPV4} != *:/* ]] && ipv4=${IPV4} || ipv4=$(curl -s -m 10 "${IPV4}")
     [[ ${IPV6} != *:/* ]] && ipv6=${IPV6} || ipv6=$(curl -s -m 10 "${IPV6}")
-    echo $IPV4
-#    if answer="$(curl -s "https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true")" && [ "${answer}" != 'KO' ]; then
-    if answer="$(curl -s "https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${DOMAIN}&myip=${ipv4}")" && [ "${answer}" != 'KO' ]; then
+    currentipaddress="$(wget -q -O - checkip.dyndns.org | sed -e 's/.Current IP Address: //' -e 's/<.$//')"
+    echo $currentipaddress
+    #if answer="$(curl -s "https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true")" && [ "${answer}" != 'KO' ]; then
+    #if answer="$(curl -s "https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${DOMAIN}&myip=${ipv4}")" && [ "${answer}" != 'KO' ]; then
+    if answer="$(curl -s --data-urlencode 'hostname=${DOMAIN}' --data-urlencode 'myip=$currentipaddress' -H 'Host: domains.google.com' -u '$USERNAME:$PASSWORD' 'https://domains.google.com/nic/update')" && [ "${answer}" != 'KO' ]; then
         bashio::log.info "${answer}"
     else
         bashio::log.warning "${answer}"
