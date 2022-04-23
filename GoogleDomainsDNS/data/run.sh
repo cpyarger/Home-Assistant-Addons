a7583b9372a5cdf1ee4f68255c9782d805853120
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-22 21:32:57 -0400

    fix missing version number

--- GoogleDomainsDNS/data/run.sh
+++ GoogleDomainsDNS/data/run.sh
@@ -42,22 +42,17 @@ if bashio::config.true 'lets_encrypt.accept_terms'; then
     fi
 fi
 
-# Run duckdns
+# Run Script
 while true; do
-
-    [[ ${IPV4} != *:/* ]] && ipv4=${IPV4} || ipv4=$(curl -s -m 10 "${IPV4}")
-    [[ ${IPV6} != *:/* ]] && ipv6=${IPV6} || ipv6=$(curl -s -m 10 "${IPV6}")
+    # Get public IP Address
     currentipaddress="$(curl icanhazip.com)"
-
-    echo $IPV4
-    #if answer="$(curl -s "https://www.duckdns.org/update?domains=${DOMAINS}&token=${TOKEN}&ip=${ipv4}&ipv6=${ipv6}&verbose=true")" && [ "${answer}" != 'KO' ]; then
-    #if answer="$(curl -s "https://${USERNAME}:${PASSWORD}@domains.google.com/nic/update?hostname=${DOMAIN}&myip=${ipv4}")" && [ "${answer}" != 'KO' ]; then
+    # Update Google Domains
     if answer="$(curl -s --data-urlencode "hostname=$DOMAIN" --data-urlencode "myip=$currentipaddress" -H "Host: domains.google.com" -u "$USERNAME:$PASSWORD" "https://domains.google.com/nic/update")" && [ "${answer}" != 'KO' ]; then
         bashio::log.info "${answer}"
     else
         bashio::log.warning "${answer}"
     fi
-
+    # Check if enough time has passed and update Let's Encrypt
     now="$(date +%s)"
     if bashio::config.true 'lets_encrypt.accept_terms' && [ $((now - LE_UPDATE)) -ge 43200 ]; then
         le_renew
