dd0e64ce04b3fda653fc80c907ebb5efd20aa169
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-29 01:58:58 -0400

    Redo comments in run.sh

--- RTLAMR-2-HA/run.sh
+++ RTLAMR-2-HA/run.sh
@@ -4,66 +4,60 @@ export LANG=C
 PATH="/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
 
 CONFIG_PATH=/data/options.json
+# Parse the variables
 AMR_MSGTYPE="$(jq --raw-output '.msgType' $CONFIG_PATH)"
 AMR_IDS="$(jq --raw-output '.ids' $CONFIG_PATH)"
-de="$(jq --raw-output '.duration_enable' $CONFIG_PATH)"
 DURATION="$(jq --raw-output '.duration' $CONFIG_PATH)"
 PT="$(jq --raw-output '.pause_time' $CONFIG_PATH)"
-# Start the listener and enter an endless loop
+
+# Print the set variables to the log
 echo "Starting RTLAMR with parameters:"
 echo "AMR Message Type =" $AMR_MSGTYPE
 echo "AMR Device IDs =" $AMR_IDS
-echo "Initial Listen Time =" $IWT
 echo "Time Between Readings =" $PT
+echo "Duration = " $DURATION
 
-
-# set -x  ## uncomment for MQTT logging...
+# Starts the RTL_TCP Application
 /usr/local/bin/rtl_tcp &
 # Sleep to fill buffer a bit
 sleep 15
 
-LASTVAL="0"
-
-# set a time to listen for. Set to 0 for unliminted
+# Function, posts data to home assistant that is gathered by the rtlamr script
 function postto {
-
 VAL="$(echo $line | jq --raw-output '.Message.Consumption' | tr -s ' ' '_')" # replace ' ' with '_'
 DEVICEID="$(echo $line | jq --raw-output '.Message.ID' | tr -s ' ' '_')"
 if [ "$DEVICEID" = "null" ]; then
   DEVICEID="$(echo $line | jq --raw-output '.Message.EndpointID' | tr -s ' ' '_')"
 fi
 RESTDATA=$( jq -nrc --arg state "$VAL" '{state: $state}')
-#echo $VAL | /usr/bin/mosquitto_pub -h $MQTT_HOST -p $MQTT_PORT -u $MQTT_USER -P $MQTT_PASS -i RTL_433 -r -l -t $MQTT_PATH
-#curl -X POST -H "Authorization: Bearer $HA_TOKEN" \
 echo -n "Sending  $RESTDATA  to http://supervisor/core/api/states/sensor.$DEVICEID -- "
 curl -s -o /dev/null -w "%{http_code}" -X POST -H "Authorization: Bearer $SUPERVISOR_TOKEN" \
 -H "Content-Type: application/json" \
 -d $RESTDATA \
 http://supervisor/core/api/states/sensor.$DEVICEID
-#$HA_HOST:$HA_PORT/api/states/sensor.$DEVICEID
 echo -e "\n"
-
 }
 
-
+# Set flags if variables are set
 x=""
-  if [ ! -z "$AMR_IDS" ]; then
-    x="-filterid=$AMR_IDS"
-  fi
+if [ ! -z "$AMR_IDS" ]; then
+  x="-filterid=$AMR_IDS"
+fi
 
-  if $de; then
-    x="$x -duration=${DURATION}s"
-  fi
-  function listener {
-   /go/bin/rtlamr -format json -msgtype=$AMR_MSGTYPE $x| while read line
-   do
-     postto
-   done
+if $de; then
+  x="$x -duration=${DURATION}s"
+fi
+# Function, runs a rtlamr listen event
+function listener {
+  /go/bin/rtlamr -format json -msgtype=$AMR_MSGTYPE $x| while read line
+  do
+    postto
+  done
 }
-# Do this loop, so will restart if buffer runs out
+
+# Main Event Loop, will restart if buffer runs out
 while true; do
   listener
   sleep $PT
-
 done
 
