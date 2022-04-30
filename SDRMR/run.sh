7b6a9793fb0a5656d85f8f06376048df1949c0cc
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-29 20:00:18 -0400

    Forgot comma

--- SDRMR/run.sh
+++ SDRMR/run.sh
@@ -54,7 +54,7 @@ function r900_parse {
   --arg unkn1 "$UNKN1" \
   --arg unkn3 "$UNKN3" \
   --arg nouse "$NOUSE" \
-  '{"state": $st, "attributes": {"unique_id": $uid, "entity_id": $uid "state_class": "total_increasing", "unit_of_measurement": "gal", "leak": $le, "leak_now": $ln, "BackFlow": $bf, "NoUse": $nouse "Unknown 1": $UNKN1, "Unknown 3": $UNKN3 }}')
+  '{"state": $st, "attributes": {"unique_id": $uid, "entity_id": $uid "state_class": "total_increasing", "unit_of_measurement": "gal", "leak": $le, "leak_now": $ln, "BackFlow": $bf, "NoUse": $nouse, "Unknown 1": $UNKN1, "Unknown 3": $UNKN3 }}')
 }
 # Function, posts data to home assistant that is gathered by the rtlamr script
 function postto {
