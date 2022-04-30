5481880bcc7f5702045c91d2b91c3fe4e8e73b1b
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-30 01:58:59 -0400

    hmm variable clash

--- SDRMR/run.sh
+++ SDRMR/run.sh
@@ -54,15 +54,15 @@ function scmplus_parse {
   STATE="$(echo $line | jq -rc '.Message.Consumption' | tr -s ' ' '_')"
   FIXED_STATE=$(($STATE/$SCMPGD))
   EPT="$(echo $line | jq -rc '.Message.EndpointType' | tr -s ' ' '_')"
-  UID=$DEVICEID-sdrmr
+  scmUID=$DEVICEID-sdrmr
   if is_gas $EPT; then
-    RESTDATA=$( jq -nrc --arg state "$FIXED_STATE" --arg uid "$UID" --arg uom "$GUOM" '{"state": $state, "attributes": {"unique_id": $uid, "state_class": "total_increasing", "device_class": "gas",  "unit_of_measurement": $uom }}')
+    RESTDATA=$( jq -nrc --arg state "$FIXED_STATE" --arg uid "$scmUID" --arg uom "$GUOM" '{"state": $state, "attributes": {"unique_id": $uid, "state_class": "total_increasing", "device_class": "gas",  "unit_of_measurement": $uom }}')
   elif is_electric $EPT; then
-    RESTDATA=$( jq -nrc --arg state "$STATE" --arg uid "$UID" --arg uom "$EUOM" '{"state": $state, "attributes": {"unique_id": $uid, "device_class": "energy", "unit_of_measurement": $uom }}')
+    RESTDATA=$( jq -nrc --arg state "$STATE" --arg uid "$scmUID" --arg uom "$EUOM" '{"state": $state, "attributes": {"unique_id": $uid, "device_class": "energy", "unit_of_measurement": $uom }}')
   elif is_water $EPT; then
-    RESTDATA=$( jq -nrc --arg state "$STATE" --arg uid "$UID" --arg uom "$WUOM"'{"state": $state, "attributes": {"unique_id": $uid}, "unit_of_measurement": $uom }')
+    RESTDATA=$( jq -nrc --arg state "$STATE" --arg uid "$scmUID" --arg uom "$WUOM"'{"state": $state, "attributes": {"unique_id": $uid}, "unit_of_measurement": $uom }')
   else
-    RESTDATA=$( jq -nrc --arg state "$STATE" --arg uid "$UID" '{"state": $state, "attributes": {"unique_id": $uid}}')
+    RESTDATA=$( jq -nrc --arg state "$STATE" --arg uid "$scmUID" '{"state": $state, "attributes": {"unique_id": $uid}}')
   fi
   }
 
