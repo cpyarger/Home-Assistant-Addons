9d68935bc2bc4f24c60cab7cce6e372ee4f262a9
Christopher P. Yarger <cpyarger@gmail.com>
2022-04-29 20:29:55 -0400

    Add support for water electric and gas meters

--- SDRMR/run.sh
+++ SDRMR/run.sh
@@ -12,6 +12,9 @@ AMR_IDS="$(jq --raw-output '.ids' $CONFIG_PATH)"
 DURATION="$(jq --raw-output '.duration' $CONFIG_PATH)"
 PT="$(jq --raw-output '.pause_time' $CONFIG_PATH)"
 GUOM="$(jq --raw-output '.gas_unit_of_measurement' $CONFIG_PATH)"
+EUOM="$(jq --raw-output '.electric_unit_of_measurement' $CONFIG_PATH)"
+WUOM="$(jq --raw-output '.water_unit_of_measurement' $CONFIG_PATH)"
+
 SCMPGD="$(jq --raw-output '.scm_plus_gas_divisor' $CONFIG_PATH)"
 # Print the set variables to the log
 echo "Starting RTLAMR with parameters:"
@@ -19,7 +22,9 @@ echo "AMR Message Type =" $AMR_MSGTYPE
 echo "AMR Device IDs =" $AMR_IDS
 echo "Time Between Readings =" $PT
 echo "Duration = " $DURATION
+echo "Electric Unit of measurement = " $EUOM
 echo "Gas Unit of measurement = " $GUOM
+echo "Water Unit of measurement = " $WUOM
 echo "SCM PLUS GAS DIVISOR = " $SCMPGD
 echo "Debug is " $DEBUG
 # Starts the RTL_TCP Application
@@ -27,17 +32,34 @@ echo "Debug is " $DEBUG
 # Sleep to fill buffer a bit
 sleep 5
 function is_gas() {
-    LIST="00, 01, 02, 09, 12, 156"
+    LIST="00, 01, 02, 09, 156"
     DELIMITER=","
     VALUE=$1
     [[ "$LIST" =~ ($DELIMITER|^)$VALUE($DELIMITER|$) ]]
 }
+function is_electric() {
+    LIST="04, 05, 07, 08"
+    DELIMITER=","
+    VALUE=$1
+    [[ "$LIST" =~ ($DELIMITER|^)$VALUE($DELIMITER|$) ]]
+}
+function is_water() {
+    LIST="03, 11, 13"
+    DELIMITER=","
+    VALUE=$1
+    [[ "$LIST" =~ ($DELIMITER|^)$VALUE($DELIMITER|$) ]]
+}
+
 function scmplus_parse {
   STATE="$(echo $line | jq -rc '.Message.Consumption' | tr -s ' ' '_')"
   FIXED_STATE=$(($STATE/$SCMPGD))
   EPT="$(echo $line | jq -rc '.Message.EndpointType' | tr -s ' ' '_')"
   if is_gas $EPT; then
     RESTDATA=$( jq -nrc --arg state "$FIXED_STATE" --arg uid "$DEVICEID" --arg uom "$GUOM" '{"unique_id": $uid, "state": $state, "attributes": { "state_class": "total_increasing", "device_class": "gas",  "unit_of_measurement": $uom }}')
+  elif is_electric $EPT; then
+    RESTDATA=$( jq -nrc --arg state "$STATE"--arg uid "$DEVICEID" --arg uom "$GUOM" '{"unique_id": $uid, "state": $state, "attributes": {"unique_id": $uid, "device_class": "energy", "unit_of_measurement": $uom }}')
+  elif is_water $EPT; then
+    RESTDATA=$( jq -nrc --arg state "$STATE"--arg uid "$DEVICEID" --arg uom "$WUOM"'{"unique_id": $uid, "state": $state, "attributes": {"unique_id": $uid}, "unit_of_measurement": $uom }')
   else
     RESTDATA=$( jq -nrc --arg state "$STATE"--arg uid "$DEVICEID" '{"unique_id": $uid, "state": $state, "attributes": {"unique_id": $uid}}')
   fi
